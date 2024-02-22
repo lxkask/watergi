8453a3a67d377484c27fc0a5d3fc7af50790742c
Lukáš Kubík <75550088+lxkask@users.noreply.github.com>
2024-02-22 18:13:34 +0100

    "Kde je problem" - update

--- src/pages/kde-je-problem/index.jsx
+++ src/pages/kde-je-problem/index.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 
 const KdeJeProblem = () => {
   return (
-    <div>
-      <div className="container mx-auto">
-        {/* Obsah stránky "Co děláme" */}
-        <h1 className="text-2xl font-bold mt-8">Kde je problém</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec,
-          mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed
-          eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit
-        </p>
+    <div className="container mx-auto px-0">
+      <div className="flex">
+        {/* Levá kostka */}
+        <div className="w-1/2 bg-violet-600 text-white p-8 flex justify-center text-center flex-col h-screen">
+          <h2 className="font-bold text-6xl mb-4">1 MINUTA = 1 MILION</h2>
+          <p className="text-lg">
+            Každou minutu se celosvětově spotřebuje 1 milion jednorázových plastových lahví.
+          </p>
+        </div>
+        {/* Pravá kostka */}
+        <div className="w-1/2 bg-cover bg-right" style={{ backgroundImage: 'url(/path/to/ocean-image.jpg)' }}>
+          <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
+            <h2 className="text-white font-bold text-2xl text-center">
+              ROK 2050, V OCEÁNECH BUDE PLAVAT VÍCE PLASTŮ NEŽ RYB
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   );
