37602901ec45af3abbd3dbbea8fd7967947324ac
Lukáš Kubík <75550088+lxkask@users.noreply.github.com>
2024-03-19 22:38:47 +0100

    "Naše řešení" update

--- src/pages/nase-reseni/index.jsx
+++ src/pages/nase-reseni/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './index.css'
-
-
-
+import waves from '../../img/waves.png';
+import staniceGenius from '../../img/staniceGenius.png';
+import waterDrop from '../../img/water-drop.svg';
 
 const NaseReseni = () => {
   return (
-    <div>
-      <h1> Tohle je nase reseni, tu se resi jak neco resit</h1>
-      
+    <div className='flex' style={{ backgroundImage: `url(${waves})`, backgroundSize: "cover" }}>
+      <div className='w-1/2 flex justify-center items-center'>
+        <img src={staniceGenius} className='h-4/5'/>
+      </div>
+      <div className='flex items-center'>
+        <ul className='text-white text-2xl list-disc font-bold bg-black px-48 py-20 bg-opacity-75 rounded-xl'>
+          <li>Výhoda 1</li>
+          <li>Výhoda 2</li>
+          <li>Výhoda 3</li>
+          <li>Výhoda 4</li>
+        </ul>
+      </div>
     </div>
   );
 };
